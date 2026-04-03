2c05b59232f886d1fdbcf37c287ce1128171107f
Brandon Mancia <bmancia001@gmail.com>
2026-04-02 23:23:32 -0500

    feat(server): add Maven wrapper, configuration files, and initial Spring Boot application setup

---
+++ server/src/main/java/io/github/shubrath0/fullstack/FullstackSetupApplication.java
@@ -0,0 +1,13 @@
+package io.github.shubrath0.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackSetupApplication.class, args);
+	}
+
+}
